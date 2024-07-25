4702b9395a5d35d14f963f16bbcab6b97ea62796
RudzaniMatidze <rdzmatidze@gmail.com>
2024-07-25 17:25:54 +0000

    file updated

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -9,34 +9,28 @@ export default class HolbertonCourse {
     this._students = students;
   }
 
-  /* Gets name of this course */
   get name() {
     return this._name;
   }
 
-  /* Sets name of this course */
   set name(value) {
     if (typeof value !== 'string') throw new TypeError('name must be a string');
     this._name = value;
   }
 
-  /* Gets length of this course */
   get length() {
     return this._length;
   }
 
-  /* Set length of this course */
   set length(value) {
     if (!Number.isInteger(value)) throw new TypeError('length must be a number');
     this._length = value;
   }
 
-  /* Gets students of this course */
   get students() {
     return this._students;
   }
 	
-  /* Sets student of this course */
   set students(value) {
     if (!Array.isArray(value)) throw new TypeError('students type must be an Array');
     this._students = value;
