7d9484d85ddfe8f1f22748e8bbbfb02770928d07
RudzaniMatidze <rdzmatidze@gmail.com>
2024-07-25 15:53:02 +0000

    Implement a class named Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
