e0ecfd70cf5191c842b3bb9b46959cf5cfbce0b5
RudzaniMatidze <rdzmatidze@gmail.com>
2024-07-30 14:49:20 +0000

    file updated

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,33 +1,22 @@
-/**
- * Updates the grades of a list of students in a given city.
- * @param {{
- *   id: Number,
- *   firstName: String,
- *   location: String
- * }[]} students - The list of students.
- * @param {String} city - The city of students.
- * @param {{
- *   studentId: Number,
- *   grade: Number,
- * }[]} newGrades - The new grades to be given to a student.
- * @returns {{id: Number, firstName: String, location: String, grade: Number | String}[]} Updated list of students with grades.
- */
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-prototype-builtins */
+export default function updateStudentGradeByCity(list, city, newGrade) {
+  return list
+    .filter((obj) => obj.location === city)
+    .map((student) => {
+    //   console.log(newGrade);
+      newGrade.map((studentGrade) => {
+        if (studentGrade.studentId === student.id) {
+          // eslint-disable-next-line no-param-reassign
+          student.grade = studentGrade.grade;
+        }
 
-  const defaultGrade = { grade: 'N/A' };
+        if (!student.hasOwnProperty('grade')) {
+          student.grade = 'N/A';
+        }
+        return student;
+      });
 
-  return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const studentGrade = newGrades.find(grade => grade.studentId === student.id) || defaultGrade;
-      return {
-        id: student.id,
-        firstName: student.firstName,
-        location: student.location,
-        grade: studentGrade.grade,
-      };
+      return student;
     });
 }
