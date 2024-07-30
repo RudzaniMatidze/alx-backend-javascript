b3450457f675462210ba87b23b289df3a601a48b
RudzaniMatidze <rdzmatidze@gmail.com>
2024-07-30 07:21:26 +0000

    function named getListStudents that returns an array of objects.

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * A function that retrieves a list of students.
+ * Return: {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
