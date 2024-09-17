3534e2bfb365c95cd218394215835052e67b57b6
RudzaniMatidze <rdzmatidze@gmail.com>
2024-09-17 08:18:27 -0700

    fix bugs

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,4 +1,4 @@
-import AppControlller from '../controllers/AppControllers';
+import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
 const mapRoutes = (app) => {
