9124517a8abf4635cdc4afc5bb99f14678b04823
RudzaniMatidze <rdzmatidze@gmail.com>
2024-09-17 07:23:37 -0700

    Task 8. Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,11 @@
+import AppControlller from '../controllers/AppControllers';
+import StudentsController from '../controllers/StudentsController';
+
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
+
+export default mapRoutes;
+module.exports = mapRoutes;
