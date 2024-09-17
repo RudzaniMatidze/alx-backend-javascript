1842ced692b10aca56f19bc798107bb660b75cf1
RudzaniMatidze <rdzmatidze@gmail.com>
2024-09-17 02:15:41 -0700

    task 0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message that prints in STDOUT.
+ * @param {string} msg - The message to display.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
